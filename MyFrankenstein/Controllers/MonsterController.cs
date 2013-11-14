f86c4c92a5f8d81cde795e3c4902b329e41da021
Steve Hentschel <slhcoding@gmail.com>
2013-11-13 19:43:58 -0500

    First commit, basic version of monster database. CRUD operations working, user login check working, database allocated. Can only edit/delete your own records. No images yet, still not pretty.

---
+++ MyFrankenstein/Controllers/MonsterController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFrankenstein.Models;
+
+namespace MyFrankenstein.Controllers
+{ 
+    public class MonsterController : Controller
+    {
+        private MonsterDBContext db = new MonsterDBContext();
+
+        //
+        // GET: /Monster/
+
+        public ViewResult Index()
+        {
+            return View(db.Monsters.ToList());
+        }
+
+        //
+        // GET: /Monster/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Monster monster = db.Monsters.Find(id);
+            return View(monster);
+        }
+
+        //
+        // GET: /Monster/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Monster/Create
+
+        [HttpPost]
+        public ActionResult Create(Monster monster)
+        {
+            if (ModelState.IsValid)
+            {
+                monster.Contributor = User.Identity.Name;       // add user name to the monster record
+                db.Monsters.Add(monster);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(monster);
+        }
+        
+        //
+        // GET: /Monster/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Monster monster = db.Monsters.Find(id);
+            return View(monster);
+        }
+
+        //
+        // POST: /Monster/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Monster monster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(monster).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(monster);
+        }
+
+        //
+        // GET: /Monster/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Monster monster = db.Monsters.Find(id);
+            return View(monster);
+        }
+
+        //
+        // POST: /Monster/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Monster monster = db.Monsters.Find(id);
+            db.Monsters.Remove(monster);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyFrankenstein/Models/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFrankenstein.Models
+{
+    public class Monster                            // Main monster class 
+    {
+        [Key]
+        public int MonsterID { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }          // type of monster
+        public string Description { get; set; }     // sentence or paragraph descriptor
+        public string ImgUrl { get; set; }
+        public string ImgName { get; set; }
+        public string ImgType { get; set; }         // image file type
+        public byte[] ImgThumb { get; set; }        // small thumbnail in db should be ok
+        public string Contributor { get; set; }     // which user submitted the entry
+    }
+
+    public class MonsterPics                        // Not sure where to put actual image yet
+    {
+        public int ImageID { get; set; }            //   may store in files (App_Data)
+        public byte[] MonsterImage { get; set; }    //   but will make room here in case of DB
+        public int MonsterID { get; set; }          // link to monster
+    }
+
+    public class MonsterDBContext : DbContext
+    {
+        public DbSet<Monster> Monsters { get; set; }
+    }
+}
