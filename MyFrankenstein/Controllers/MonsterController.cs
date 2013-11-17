8f93f5c70956571c349c9164203485f403200462
Steve Hentschel <slhcoding@gmail.com>
2013-11-17 08:48:59 -0500

    Rev 1.2.0 updates (11/17/13) See ReadMe for details.  slh

--- MyFrankenstein/Controllers/MonsterController.cs
+++ MyFrankenstein/Controllers/MonsterController.cs
@@ -41,40 +41,60 @@ namespace MyFrankenstein.Controllers
 
         //
         // POST: /Monster/Create
-
+        
         [HttpPost]
         public ActionResult Create(Monster monster, HttpPostedFileBase file)
         {
-                try
-                {
-                    if (file != null && file.ContentLength > 0)
-                    {
-                        var fileName = Path.GetFileName(file.FileName);
-                        monster.ImgName = fileName;
-                        monster.ImgType = Path.GetExtension(file.FileName);
-                        var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
-                        monster.ImgUrl = path;          // Local Url, what's the point to save ?
+            if (file != null)                               // If file was specified, do error checks
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                monster.ImgName = fileName;
+                monster.ImgType = Path.GetExtension(file.FileName);
 
+                if (!ImageType(monster.ImgType)) {
+                    ModelState.AddModelError("", "Specified file is not an image type; please try a PNG, GIF, or JPG file.");
+                    return View (monster);
+                }
+                else if (file.ContentLength == 0) {
+                    ModelState.AddModelError("", "Specified file is empty, sorry.");
+                    return View (monster);
+                }
+                else if (file.ContentLength > 1 * 1024 *1024) {
+                    ModelState.AddModelError("", "Specified file is too big; please keep it less than 1Mb.");
+                    return View (monster);
+                }
+                else {
+                    try {
+                        var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
+                        monster.ImgUrl = path;          // Local Url, keep for deleting purposes
                         file.SaveAs(path);
                     }
-                    ViewBag.Message = "Upload successful";
-  //                  return RedirectToAction("Create");
+                    catch {
+                        ModelState.AddModelError("", "Image file upload failed.");
+                        return View(monster);
+                    }
                 }
-                catch
-                {
-                    ViewBag.Message = "Image file upload failed."; 
-                    return View(monster);
-                }
-            
-            if (ModelState.IsValid)
+            }
+
+            if (ModelState.IsValid)                             // default add record, w/ or w/o image
             {
                 monster.Contributor = User.Identity.Name;       // add user name to the monster record
                 db.Monsters.Add(monster);
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
 
+            ModelState.AddModelError("", "Unknown error creating record in Database.");
             return View(monster);
+        }
+
+        public static bool ImageType(string imgExt)             // my humble image ext checker (I saw much fancier on Google)
+        {
+            var allowedExtensions = new[] { ".png", ".gif", ".jpg", ".jpeg" };
+
+            if (allowedExtensions.Contains(imgExt)) return true;
+            else return false;
         }
         
         //
@@ -92,25 +112,40 @@ namespace MyFrankenstein.Controllers
         [HttpPost]
         public ActionResult Edit(Monster monster, HttpPostedFileBase file)
         {
-            try
-            {   
-                if (file != null && file.ContentLength > 0)
-                {
-                    var fileName = Path.GetFileName(file.FileName);
+            if (file != null)                                // If file was specified, do error checks
+            {
+                var fileName = Path.GetFileName(file.FileName);
+
+                if (fileName != monster.ImgName) {          // but only do checks if input name is different       
                     monster.ImgName = fileName;
                     monster.ImgType = Path.GetExtension(file.FileName);
-                    var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
-                    monster.ImgUrl = path;          // Local Url, what's the point to save in DB ?
 
-                    file.SaveAs(path);
+                    if (!ImageType(monster.ImgType)) {
+                        ModelState.AddModelError("", "Specified file is not an image type; please try a PNG, GIF, or JPG file.");
+                        return View(monster);
+                    }
+                    else if (file.ContentLength == 0) {
+                        ModelState.AddModelError("", "Specified file is empty, sorry.");
+                        return View(monster);
+                    }
+                    else if (file.ContentLength > 1 * 1024 * 1024) {
+                        ModelState.AddModelError("", "Specified file is too big; please keep it less than 1Mb.");
+                        return View(monster);
+                    }
+                    else {
+                        var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
+                        try {
+                            file.SaveAs(path);
+                        }
+                        catch {
+                            ModelState.AddModelError("", "Image file upload failed.");
+                            return View(monster);
+                        }
+
+                        System.IO.File.Delete(monster.ImgUrl);          // Delete previous image file
+                        monster.ImgUrl = path;                          // Set new file name for db
+                    }
                 }
-                ViewBag.Message = "Upload successful";
-                //                  return RedirectToAction("Edit");
-            }
-            catch
-            {
-                ViewBag.Message = "Image file upload failed.";
-                return View(monster);
             }
 
             if (ModelState.IsValid)                                 // Update the record
@@ -119,6 +154,8 @@ namespace MyFrankenstein.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
+            ModelState.AddModelError("", "Unknown error saving to Database.");
             return View(monster);
         }
 
@@ -140,6 +177,18 @@ namespace MyFrankenstein.Controllers
             Monster monster = db.Monsters.Find(id);
             db.Monsters.Remove(monster);
             db.SaveChanges();
+
+            if (monster.ImgName != "")                          // Get rid of image file (if exists)
+            {                                                    
+                try {
+                    System.IO.File.Delete(monster.ImgUrl);
+                }
+                catch {
+                    //  need some error processing here, but doesn't matter too much 
+                    //  record is gone, and user can't do much about deleting the file on server
+                    //      (good case for putting the image in the db next time)
+                }
+            }
             return RedirectToAction("Index");
         }
 
--- README.md
+++ README.md
@@ -3,10 +3,17 @@ My-Frankenstein
 
 My first pet project: a Monster Database using C# .NET MVC3 w/ SQL Express in Visual Studio 2010.
 
+MF Rev 1.2.0 -- (11/17/13)
+- Added image file upload/change process to Edit; added file error processing to Edit/Create
+- Added image file type check (png, jpg, gif) and file size check (<1Mb) with error msgs
+- Now deletes old image file from ~/Content/Images on Delete or when image is changed in Edit
+- Added 'redirect to calling view' when logging in (harder than it sounds)
+-- Still need to add thumbnail processing, Quick Index, and then beautify.  slh
+
 MF Rev 1.1.0 -- (11/15/13)
 - Added image file upload process to Create (still working on Edit)
 - Added image displays to Details and Delete views
--- Working on image file in Edit (no upload if not changed), and deleting image file when monster record is Deleted
+-- Working on image file in Edit (no upload if not changed), and deleting image file when monster record is Deleted.  slh
 
 MF Rev 1.0.0 -- Base (11/13/13)
 - Initial monster database, allocated DB and classes, basic CRUD working
